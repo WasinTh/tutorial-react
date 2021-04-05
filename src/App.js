4907a5e7a9f78044269a8e6e925f2687c4abd5c3
Terry <wasin.th@gmail.com>
2021-04-05 11:19:01 +0700

    Add static HTML table

--- src/App.js
+++ src/App.js
@@ -5,18 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table border="1">
+          <tr>
+            <th>Date-Time</th>
+            <th>Type</th>
+            <th>amount</th>
+            <th>note</th>
+          </tr>
+          <tr>
+            <td>01/02/2021 - 08:30</td>
+            <td>รายรับ</td>
+            <td>20,000</td>
+            <td>Allowance</td>
+          </tr>
+          <tr>
+            <td>01/02/2021 - 10:30</td>
+            <td>รายจ่าย</td>
+            <td>150</td>
+            <td>อาหารเที่ยง</td>
+          </tr>
+        </table>
       </header>
     </div>
   );
