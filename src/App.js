09e70475134eb058f66b3541a3f8b0b73a1627c4
Terry <wasin.th@gmail.com>
2021-04-05 11:28:14 +0700

    Conditional Rendering 1

--- src/App.js
+++ src/App.js
@@ -8,9 +8,20 @@ function App() {
     { id: 2, created: "01/02/2021 - 10:30", type: "รายจ่าย", amount: 150, note: "อาหารเที่ยง" },
   ]
 
+  const summary = () => {
+    const currentAmount = transactionData.reduce( (sum, transaction) => sum += transaction.amount, 0);
+    if(currentAmount > 10000) {
+      return <p style={{'color': 'green'}}>Wow you're so rich - {currentAmount}</p>
+    }
+    else {
+      return <p style={{ 'color': 'red' }}>So poor... - {currentAmount}</p>
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        {summary()}
         <TransactionList data={transactionData} />
       </header>
     </div>
