6d53635514a1b98a7f2ce0bf9dd2545de509d0b6
Terry <wasin.th@gmail.com>
2021-04-05 11:16:03 +0700

    Add setinterval to ticking the clock

--- src/index.js
+++ src/index.js
@@ -4,15 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const showTime = () => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
+
+setInterval(showTime, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
